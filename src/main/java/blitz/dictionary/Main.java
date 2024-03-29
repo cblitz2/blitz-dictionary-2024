4374686f23e653974488203e3f044b27140cf322
chanablitz <cblitz2@student.touro.edu>
2024-03-29 10:30:20 -0400

    added unit test

--- src/main/java/blitz/dictionary/Main.java
+++ src/main/java/blitz/dictionary/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws CsvValidationException, IOException {
-
         SwingUtilities.invokeLater(() -> {
             try {
                 new DictionaryFrame().setVisible(true);
--- src/test/java/blitz/dictionary/EnglishDictionaryTest.java
+++ src/test/java/blitz/dictionary/EnglishDictionaryTest.java
@@ -1,21 +1,25 @@
 package blitz.dictionary;
 
+import com.opencsv.exceptions.CsvValidationException;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EnglishDictionaryTest {
 
-//    @Test
-//    public void getDefinition() {
-//        // given
-//        EnglishDictionary dictionary = new EnglishDictionary();
-//
-//        // when
-//        List<String> definitions = dictionary.getDefinition("");
-//
-//        // then
-//        assert...
-//    }
+    @Test
+    public void getDefinition() throws CsvValidationException, IOException {
+        // given
+        EnglishDictionary dictionary = new EnglishDictionary();
+        String testWord = "Zealotist";
+
+        // when
+        List<String> definitions = dictionary.getDefinition(testWord);
+
+        // then
+        assertTrue(definitions.contains("A zealot."));
+    }
 
 }
